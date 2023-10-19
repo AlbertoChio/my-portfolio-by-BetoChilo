e31b0deb550d84770e6f86c4af65981e6f9347a5
Beto ChioLo <a.chio@outlook.com>
2023-10-19 00:31:12 -0600

    about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, HostBinding, signal } from '@angular/core';
+import {  } from '@fortawesome/fontawesome-svg-core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Beto_ChiLo_Port_Folio';
+  darkMode = signal<boolean>(false);
+  @HostBinding('class.dark') get mode() {return this.darkMode(); }
+  showMenu = false;
+  toggleNavbar(){
+    this.showMenu = !this.showMenu;
+  }
 }
