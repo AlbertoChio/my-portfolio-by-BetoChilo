bb1204b638126cc5d4093ece9a128649bdd9955e
Beto ChioLo <a.chio@outlook.com>
2023-10-19 23:21:36 -0600

    my experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,15 @@ export class AppComponent {
   darkMode = signal<boolean>(false);
   @HostBinding('class.dark') get mode() {return this.darkMode(); }
   showMenu = false;
+  cards:{description:string,src:string}[]=[
+    {src:'assets/images/svg/angular-icon.svg',description:'Angular'},
+    {src:'assets/images/svg/spring-3.svg',description:'Java Spring'},
+    {src:'assets/images/svg/bootstrap-5-1.svg',description:'Bootstrap'},
+    {src:'assets/images/svg/tailwindcss.svg',description:'Tailwindcss'},
+    {src:'assets/images/svg/google-analytics-4.svg',description:'Google Analytics'},
+    {src:'assets/images/svg/gitignoreio-1.svg',description:'Git'},
+    {src:'assets/images/svg/jenkins-1.svg',description:'Jenkins'},
+  ]
   toggleNavbar(){
     this.showMenu = !this.showMenu;
   }
